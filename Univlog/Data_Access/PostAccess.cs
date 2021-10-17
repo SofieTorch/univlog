29bf5c4270604d1b6dd5b1c9281b56b0a3f3a607
Sofia Toro Chambi <tcs0028814@est.univalle.edu>
2021-10-17 04:19:07 -0400

    Retrieving subjects posts from db to subjects page and showing it

---
+++ Univlog/Data_Access/PostAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Univlog.Models;
+using WebMatrix.Data;
+
+namespace Univlog.Data_Access
+{
+    public class PostAccess
+    {
+        public IEnumerable<Post> GetSubjectsPosts()
+        {
+            List<Post> res = new List<Post>();
+            Database conn = Database.Open("UnivlogDB");
+            string query = "SELECT * FROM Post WHERE Topic = 1";
+
+            foreach (var row in conn.Query(query))
+                res.Add(new Post(row.PostId, row.Title, row.Topic));
+
+            return res;
+        }
+
+        public IEnumerable<Post> GetTeachersPosts()
+        {
+            List<Post> res = new List<Post>();
+            Database conn = Database.Open("UnivlogDB");
+            string query = "SELECT * FROM Post WHERE Topic = 0";
+
+            foreach (var row in conn.Query(query))
+                res.Add(new Post(row.PostId, row.Title, row.Topic));
+
+            return res;
+        }
+    }
+}
---
+++ Univlog/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Univlog.Models
+{
+    public class Post
+    {
+        public short PostId { private set; get; }
+        public string Title { private set; get; }
+        public byte Topic{ private set; get; }
+
+        public Post(short postId, string title, byte topic)
+        {
+            PostId = postId;
+            Title = title;
+            Topic = topic;
+        }
+    }
+}
