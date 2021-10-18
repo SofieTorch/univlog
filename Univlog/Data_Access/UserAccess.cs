dbda6c93b76532b84b32eb59b8f653b91aec2fa3
Daniel <dansan1234567@gmail.com>
2021-10-18 17:14:13 -0400

    LogIn - SignUp

--- Univlog/Data_Access/UserAccess.cs
+++ Univlog/Data_Access/UserAccess.cs
@@ -9,20 +9,58 @@ namespace Univlog.Data_Access
 {
     public class UserAccess
     {
-        public User Get(int id)
+        public bool Add(User user)
         {
             Database conn = Database.Open("UnivlogDB");
-            string query = "SELECT * FROM [User] WHERE UserId = @0";
+            string query = @"INSERT INTO [User](Name, PaternalSurname, MaternalSurname, Email, Password, Role) 
+                             VALUES (@0, @1, @2, @3, HashBytes('MD5', @4), @5)";
 
-            var row = conn.QuerySingle(query, id);
-            User res = new User(row.UserId);
-            res.Name = row.Name;
-            res.PaternalSurname = row.PaternalSurname;
-            res.MaternalSurname = row.MaternalSurname;
-            res.Email = row.Email;
-            res.RoleCode = row.Role;
+            int rowsAffected = conn.Execute(query, user.Name, user.PaternalSurname, user.MaternalSurname, user.Email, user.Password, user.RoleCode);
+            bool success = rowsAffected == 1;
+
+            return success;
+        }
+
+        public User Get(short id)
+        {
+            User res = null;
+            try
+            {
+                Database conn = Database.Open("UnivlogDB");
+                string query = "SELECT * FROM [User] WHERE UserId = @0";
+
+                var row = conn.QuerySingle(query, id);
+                res = new User(row.UserId, row.Name, row.PaternalSurname, row.MaternalSurname, row.Email, row.Role);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
             return res;
         }
+
+        public User LogIn(string email, string password)
+        {
+            User res = null;
+            try
+            {
+                Database conn = Database.Open("UnivlogDB");
+                string query = "SELECT * FROM [User] WHERE Email = @0 AND Password = HashBytes('MD5', @1)";
+
+                var row = conn.QuerySingle(query, email, password);
+                res = new User(row.UserId, row.Name, row.PaternalSurname, row.MaternalSurname, row.Email, row.Role);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }            
+
+            return res;
+        }
+
+
     }
 }
